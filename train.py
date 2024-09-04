20db9c34357f1267e0de42779e4098a02e066e3a
leeyoungchun-xiilab <yc.lee@xiilab.com>
2024-09-04 15:12:37 +0900

    first commit

---
+++ train.py
@@ -0,0 +1,106 @@
+import argparse
+import os
+import csv
+from ultralytics import YOLO
+from ultralytics.utils.astrago_db import MariaDBHandler
+from ultralytics.utils.astrago import Astrago
+
+
+# model freeze
+def freeze_layer(trainer):
+    model = trainer.model
+    num_freeze = 10
+    print(f"Freezing {num_freeze} layers")
+    freeze = [f'model.{x}.' for x in range(num_freeze)]  # layers to freeze 
+    for k, v in model.named_parameters():
+        v.requires_grad = True  # train all layers 
+        if any(x in k for x in freeze):
+            print(f'freezing {k}')
+            v.requires_grad = False
+    print(f"{num_freeze} layers are freezed.")
+
+
+# save dir filter same folder
+
+def trainer(config):
+    # Load a model
+    # model = YOLO('yolov8l.yaml')  # build a new model from YAML
+    # model = YOLO('yolov8l.pt')  # load a pretrained model (recommended for training)
+    model = YOLO(config['model']).load(config['model_pt'])  # build from YAML and transfer weights
+    model.add_callback("on_train_start", freeze_layer)
+
+    # Train the model
+    results = model.train(
+        data=config['data_dir'],
+        imgsz=config['image_size'],
+        epochs=config['epochs'],
+        batch=config['batch_size'],
+        lr0=config['learning_rate'],
+        lrf=config['learning_rate'],
+        project=config['save_model_dir'],
+
+        device=[0],
+        workers=config['worker'],
+        optimizer=config['opt'],
+        pretrained=config['pretrained'],
+        single_cls=config['single_cls'],
+        label_smoothing=config['label_smoothing'],
+        patience=config['patience'],
+    )
+    model.val()
+    db_handler = MariaDBHandler(
+        host=os.environ.get("DB_HOST"),
+        port=os.environ.get("DB_PORT"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        database=os.environ.get("DB_DATABASE"),
+    )
+    db_handler.connect()
+    parameter_id = db_handler.insert_prediction_parameter((config['model'], config['model_pt'], config['data_dir'],
+                                                           config['image_size'], config['epochs'], config['batch_size'],
+                                                           config['learning_rate'], config['save_model_dir'],
+                                                           config['patience'], config['worker'], config['opt'],
+                                                           config['single_cls'], config['label_smoothing'],
+                                                           config['pretrained']))
+    with open(Astrago.csv_file_path, 'r') as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)
+        for row in csv_reader:
+            db_handler.insert_epoch_log(parameter_id, (row[0], row[1], row[2], row[3], row[4],
+                                                       row[5], row[6], row[7], row[8], row[9],
+                                                       row[10], row[11], row[12], row[13], row[14],
+                                                       row[15], row[16], row[17], row[18], row[19],
+                                                       row[20], row[21]
+                                                       ))
+    db_handler.disconnect()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='YOLOv8 training')
+    # user setting
+    parser.add_argument('--data_dir', type=str,
+                        default="./ultralytics/cfg/datasets/coco128.yaml",
+                        help="yaml file")
+    parser.add_argument('--image_size', type=int, default=640, help="input image scale")
+    parser.add_argument('--epochs', type=int, default=100, help="num epochs")
+    parser.add_argument('--batch_size', type=int, default=16, help="number of images per batch (-1 for AutoBatch)")
+    parser.add_argument('--learning_rate', type=float, default=0.01, help="output checkpoint, info save_dir")
+    parser.add_argument('--save_model_dir', type=str, default="./default_train_result", help="output checkpoint, info save_dir")
+
+    parser.add_argument('--model', type=str, default="./models/yolov8/yaml/yolov8l.yaml",
+                        help="model .yaml default yolov8n.yaml")
+    parser.add_argument('--model_pt', type=str, default="./models/yolov8/pt/yolov8l.pt", help="pre-trained model path")
+    parser.add_argument('--patience', type=int, default=0, help='EarlyStopping patience (epochs without improvement)')
+    parser.add_argument('--worker', type=int, default=8,
+                        help="number of worker threads for data loading (per RANK if DDP)")
+    parser.add_argument('--pretrained', type=bool, default=False, help="whether to use a pretrained model")
+    parser.add_argument('--opt', type=str, default='SGD',
+                        help="optimizer to use, choices=['SGD', 'Adam', 'AdamW', 'RMSProp']")
+    parser.add_argument('--single_cls', action='store_true', help='train multi-class data as single-class')
+    parser.add_argument('--label_smoothing', type=float, default=0.0, help='Label smoothing epsilon')
+    return vars(parser.parse_args())
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    trainer(args)
---
+++ ultralytics/utils/astrago_db.py
@@ -0,0 +1,254 @@
+import os
+
+import mysql.connector
+from kubernetes import client, config
+
+
+class KubernetesInfo:
+
+    def __init__(self):
+        config.load_incluster_config()
+        self.pod_name = os.environ.get("POD_NAME")
+        self.namespace = os.environ.get("POD_NAMESPACE")
+        self.core_v1_api = client.CoreV1Api()
+        self.batch_v1_api = client.BatchV1Api()
+
+    def _change_job_annotation(self, job_name, key, value):
+        job = self.batch_v1_api.read_namespaced_job(name=job_name, namespace=self.namespace)
+
+        # 기존 annotation에 새로운 값을 추가 또는 업데이트
+        if job.metadata.annotations is None:
+            job.metadata.annotations = {}
+        job.metadata.annotations[key] = str(value)
+
+        # Pod 업데이트
+        self.batch_v1_api.patch_namespaced_job(name=job_name, namespace=self.namespace, body=job)
+        print("JOB의 annotation이 성공적으로 업데이트되었습니다.")
+
+    def get_job_name(self):
+        try:
+            # Pod 조회
+            pod = self.core_v1_api.read_namespaced_pod(name=self.pod_name, namespace=self.namespace)
+            job_name = pod.metadata.owner_references[0].name
+            return job_name
+        except Exception as e:
+            print(f"JOB annotation 업데이트 중 오류 발생: {e}")
+            return ""
+
+    def change_remaining_time_annotation(self, estimated_remaining_time):
+        try:
+            # Pod 조회
+            pod = self.core_v1_api.read_namespaced_pod(name=self.pod_name, namespace=self.namespace)
+            job_name = pod.metadata.owner_references[0].name
+            self._change_job_annotation(job_name, 'estimated_remaining_time', estimated_remaining_time)
+        except Exception as e:
+            print(f"JOB annotation 업데이트 중 오류 발생: {e}")
+
+    def change_initial_time_annotation(self, estimated_initial_time):
+        try:
+            # Pod 조회
+            pod = self.core_v1_api.read_namespaced_pod(name=self.pod_name, namespace=self.namespace)
+            job_name = pod.metadata.owner_references[0].name
+            self._change_job_annotation(job_name, 'estimated_initial_time', estimated_initial_time)
+        except Exception as e:
+            print(f"JOB annotation 업데이트 중 오류 발생: {e}")
+
+    def get_node_port(self):
+        # Kubernetes 클러스터 설정
+        config.load_incluster_config()
+        v1 = client.CoreV1Api()
+        # 현재 파드의 네임스페이스와 파드 이름 가져오기
+        namespace = open("/var/run/secrets/kubernetes.io/serviceaccount/namespace").read()
+        pod_name = open("/var/run/secrets/kubernetes.io/serviceaccount/pod-name").read()
+
+        # 서비스 이름과 노드 포트 가져오기
+        service_name = "astrago-mariadb"  # 원하는 서비스 이름
+        node_port = None
+
+        # 서비스 목록 가져오기
+        services = v1.list_namespaced_service(namespace)
+        for svc in services.items:
+            if svc.metadata.name == service_name:
+                node_port = svc.spec.ports[0].node_port
+                break
+
+        print(f"서비스 이름: {service_name}")
+        print(f"노드 포트: {node_port}")
+
+
+class MariaDBHandler:
+    def __init__(self, host, port, user, password, database):
+        """
+        MariaDBHandler 클래스의 생성자 메서드입니다.
+
+        :param host: MariaDB 호스트 주소
+        :param user: MariaDB 사용자 이름
+        :param password: MariaDB 암호
+        :param database: 연결할 데이터베이스 이름
+        """
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.port = port
+        self.conn = None
+        self.cursor = None
+
+    def connect(self):
+        """
+        MariaDB에 연결하는 메서드입니다.
+        """
+        try:
+            self.conn = mysql.connector.connect(
+                host=self.host,
+                user=self.user,
+                port=self.port,
+                password=self.password,
+                database=self.database
+            )
+            self.cursor = self.conn.cursor()
+            print("MariaDB에 연결되었습니다.")
+        except mysql.connector.Error as e:
+            print(f"MariaDB 연결 오류: {e}")
+
+    def disconnect(self):
+        """
+        MariaDB 연결을 해제하는 메서드입니다.
+        """
+        if self.conn.is_connected():
+            self.cursor.close()
+            self.conn.close()
+            print("MariaDB 연결이 닫혔습니다.")
+
+    def insert_epoch_log(self, parameter_id, values):
+        """
+        TB_JOB_PREDICTION_EPOCH_LOG 테이블에 데이터를 삽입하는 메서드입니다.
+
+        :param values: 삽입할 데이터 값
+        """
+        try:
+            # 데이터 삽입 쿼리 생성
+            sql = """
+            INSERT INTO TB_JOB_PREDICTION_EPOCH_LOG 
+            (JOB_PREDICTION_PARAMETER_ID, MODEL_NAME, MODEL_PARAM_NUM,
+             GPU_TYPE, GPU_FLOPS, CLASS_NUM, TRAIN_IMG_NUM, TRAIN_INSTANCE_NUM, VALID_IMG_NUM, VALID_INSTANCE_NUM,
+             IMG_SIZE, BATCH_SIZE, EPOCH_CNT, PREPROCESS_TIME, TRAIN_TIME, VALID_TIME, TIME_PER_EPOCH, SCHEDULER_TIME,
+             EPOCH_TIME, ELAPSED_TIME, REMAINING_TIME, GPU_USAGE, CPU_USAGE) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """
+
+            # 데이터 삽입
+            self.cursor.execute(sql, (parameter_id,) + values)
+
+            # 변경사항 커밋
+            self.conn.commit()
+            print("데이터가 성공적으로 삽입되었습니다.")
+        except Exception as e:
+            print(f"데이터 삽입 중 오류 발생: {e}")
+            # 롤백
+            self.conn.rollback()
+
+    def insert_prediction_parameter(self, values):
+        """
+        TB_JOB_PREDICTION_PARAMETER 테이블에 데이터를 삽입하는 메서드입니다.
+
+        :param values: 삽입할 데이터 값
+        """
+        try:
+            # 데이터 삽입 쿼리 생성
+            sql = """
+            INSERT INTO TB_JOB_PREDICTION_PARAMETER 
+            (MODEL, MODEL_PT, DATA_DIR, IMAGE_SIZE, EPOCH, BATCH_SIZE, LEARNING_RATE,
+             SAVE_MODEL_DIR, PATIENCE, WORKER, OPT, SINGLE_CLS, LABEL_SMOOTHING, PRETRAINED) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """
+
+            # 데이터 삽입
+            self.cursor.execute(sql, values)
+
+            # 변경사항 커밋
+            self.conn.commit()
+            print("데이터가 성공적으로 삽입되었습니다.")
+            return self.cursor.lastrowid
+        except Exception as e:
+            print(f"데이터 삽입 중 오류 발생: {e}")
+            # 롤백
+            self.conn.rollback()
+    def update_workload_job_remain_time(self, time, workloadResourceName):
+        """
+        TB_WORKLOAD 테이블에 남은시간을 업데이트하는 메서드입니다.
+        :param values: 삽입할 데이터 값
+        """
+        try:
+            # 데이터 삽입 쿼리 생성
+            sql = """
+            UPDATE TB_WORKLOAD_JOB twj
+            JOIN TB_WORKLOAD tw ON tw.WORKLOAD_ID = twj.WORKLOAD_ID 
+            SET twj.REMAIN_TIME = %s
+            WHERE tw.WORKLOAD_RESOURCE_NAME = %s
+            """
+            # 데이터 삽입
+            self.cursor.execute(sql, (time, workloadResourceName))
+            # 변경사항 커밋
+            self.conn.commit()
+            print("데이터가 성공적으로 업데이트 되었습니다.")
+            return self.cursor.lastrowid
+        except Exception as e:
+            print(f"데이터 삽입 중 오류 발생: {e}")
+            # 롤백
+            self.conn.rollback()
+
+
+    def update_workload_distributed_remain_time(self, time, workloadResourceName):
+        """
+        TB_WORKLOAD 테이블에 남은시간을 업데이트하는 메서드입니다.
+        :param values: 삽입할 데이터 값
+        """
+        try:
+            # 데이터 삽입 쿼리 생성
+            sql = """
+            UPDATE TB_WORKLOAD_DISTRIBUTED_JOB twdj
+            JOIN TB_WORKLOAD tw ON tw.WORKLOAD_ID = twdj.WORKLOAD_ID 
+            SET twdj.REMAIN_TIME = %s
+            WHERE tw.WORKLOAD_RESOURCE_NAME = %s
+            """
+            # 데이터 삽입
+            self.cursor.execute(sql, (time, workloadResourceName))
+            # 변경사항 커밋
+            self.conn.commit()
+            print("데이터가 성공적으로 업데이트 되었습니다.")
+            return self.cursor.lastrowid
+        except Exception as e:
+            print(f"데이터 삽입 중 오류 발생: {e}")
+            # 롤백
+            self.conn.rollback()
+
+    def find_workload_type(self, workloadResourceName):
+        """
+        TB_WORKLOAD 테이블에서 워크로드 타입을 조회하는 메서드입니다.
+        """
+        try:
+            # 데이터 삽입 쿼리 생성
+            sql = """
+            SELECT tw.WORKLOAD_TYPE
+            FROM TB_WORKLOAD tw 
+            WHERE WORKLOAD_RESOURCE_NAME = %s
+            """
+            # 데이터 삽입
+            self.cursor.execute(sql, (workloadResourceName,))
+            result = self.cursor.fetchone()
+            if result:
+            # 결과 반환
+                return result[0]
+            else:
+            # 결과가 없을 때 None 반환
+                return None
+        except Exception as e:
+            print(f"데이터 조회 중 오류 발생: {e}")
+            return None
+
+
+if __name__ == '__main__':
+    client = KubernetesInfo()
+    client.change_estimated_initial_time(10)
+    client.change_remaining_time_annotation(10)
